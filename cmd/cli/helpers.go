0b8524cf5d2df7c39d6c702b71d18a3772cb27e6
ploMP4 <kostasarto2@gmail.com>
2022-01-28 00:09:42 +0200

    created clear screen function, updated help

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -3,9 +3,36 @@ package main
 import (
 	"errors"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/fatih/color"
 )
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func ClearScreen() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		color.Red("Screen clearing is unsupported on your platform")
+	}
+}
 
 func setup(command, appName string) {
 	if command != "version" && command != "help" {
@@ -29,9 +56,11 @@ func appNameExists(appName string) {
 func showHelp() {
 	color.Yellow(`Available commands:
 	
-	help                           -show the help menu
-	version                        -print application version
-	cpp <appname> || c++ <appname> -create c++ application 
+	help                                                                                   -show the help menu
+	version                                                                                -print application version
+	cpp <appname> || c++ <appname>                                                         -create c++ application 
+	react <appname> [ts | typescript] [redux] [mui | material-ui | materialUI] [bootstrap] -create react app and optionally add typescript, redux, material-ui, bootstrap
+	next <appname> [ts | typescript] [redux]                                               -crete nextJS app and optionally add typescript, redux  
 
 	`)
 }
