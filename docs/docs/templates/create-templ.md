99472f10ff4385e67f62ca9eec7a73cdad2d0baa
ploMP4 <kostasarto2@gmail.com>
2023-08-05 12:51:06 +0300

    docs: update docs

--- docs/docs/templates/create-templ.md
+++ docs/docs/templates/create-templ.md
@@ -3,3 +3,51 @@ sidebar_position: 2
 ---
 
 # Create a template
+
+A template is basically just a file that gets copied into your project.
+
+So let's say we want to create a starter file for our c++ project to use
+as a template. As stated before template files need to be under a directory
+with the same name as the command.
+
+Let's say we name our command cpp,
+we will create a directory in the `templates folder` called `cpp`.
+
+**When we say that a file is going to use a template in our [config](#files) file**
+**we specify a path for that file so in our templates folder we need to follow the same directory structure.**
+
+For example let's say we want to have a directory named `src` that will
+store our source files, and in there we want to insert our `main.cpp template`.
+Based on that we will need to create a directory with the name `src` inside
+our template folder for the command we made called `cpp`.
+
+```bash
+mkdir src
+```
+
+Now we can create our template file inside the src directory.
+
+```bash
+cd src && touch main.cpp
+```
+
+```cpp title="src/main.cpp"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
+```
+
+Now we can add the template file under our desired subcommand like so:
+
+```yaml title="cpp.yaml"
+subCommands:
+    cpp-subcommand:
+        files:
+            src:
+                filepath: "src/main.cpp"
+                template: true
+```
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,50 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'GoGo My Project',
-  tagline: 'Scaffold any project in seconds',
-  favicon: 'img/favicon.ico',
+  title: "GoGo My Project",
+  tagline: "Scaffold any project in seconds",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ploMP4', // Usually your GitHub org/user name.
-  projectName: 'GoGo-MyProject', // Usually your repo name.
+  organizationName: "ploMP4", // Usually your GitHub org/user name.
+  projectName: "GoGo-MyProject", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'github.com/ploMP4/GoGo-MyProject',
+          editUrl: "github.com/ploMP4/GoGo-MyProject",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -54,66 +54,72 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'GoGo My Project',
+        title: "GoGo My Project",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
           },
+          // {
+          //   href: "https://github.com/ploMP4/GoGo-MyProject",
+          //   label: "GitHub",
+          //   position: "right",
+          // },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            type: "html",
+            position: "right",
+            value:
+              '<a href="https://github.com/ploMP4/GoGo-MyProject" target="_blank" class="header-github-link">',
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
