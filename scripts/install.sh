99c366f95ba6cb9e40c6710c45ab1520a92a1f0a
plo <kostasarto2@gmail.com>
2023-08-22 19:11:35 +0300

    fix: create gadget & template dir from install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,21 +5,25 @@ DIST_PATH=./dist/gogo
 APPLICATION_PATH=$HOME/.gogo
 BINARY_PATH=$APPLICATION_PATH/bin
 SETTINGS_FILE_PATH=$BINARY_PATH/settings.yaml
+GADGET_PATH=$APPLICATION_PATH/gadgets
+TEMPLATES_PATH=$APPLICATION_PATH/templates
 
 go build -o ./dist/gogo ./cmd/...
 
-if [ ! -d $APPLICATION_PATH ]; then 
-    mkdir $APPLICATION_PATH
-    mkdir $BINARY_PATH
+if [ ! -d "$APPLICATION_PATH" ]; then 
+    mkdir "$APPLICATION_PATH"
+    mkdir "$BINARY_PATH"
+    mkdir "$GADGET_PATH"
+    mkdir "$TEMPLATES_PATH"
 fi
 
-mv $DIST_PATH $BINARY_PATH
+mv $DIST_PATH "$BINARY_PATH"
 
-# rmdir ./dist
+rm -rf ./dist
 
-if [ ! -f $SETTINGS_FILE_PATH ]; then 
-    touch $SETTINGS_FILE_PATH
+if [ ! -f "$SETTINGS_FILE_PATH" ]; then 
+    touch "$SETTINGS_FILE_PATH"
 
-    echo 'gadget-path: "'$APPLICATION_PATH'/gadgets"
-    template-path: "'$APPLICATION_PATH'/templates"' > $SETTINGS_FILE_PATH
+    echo 'gadget-path: "'$APPLICATION_PATH'/gadgets"' > "$SETTINGS_FILE_PATH"
+    echo 'template-path: "'$APPLICATION_PATH'/templates"' >> "$SETTINGS_FILE_PATH"
 fi
