49f0f93c910e66868de3f5ef48f7dbf3114781e5
plo <kostasarto2@gmail.com>
2023-08-23 11:44:54 +0300

    scripts: delete install.sh and create install.py instead

---
+++ scripts/install.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+DIST_PATH = Path(f"{Path.cwd()}/dist/gogo")
+
+APPLICATION_PATH = Path(f"{Path.home()}/.gogo")
+BINARY_PATH = Path(f"{APPLICATION_PATH}/bin")
+SETTINGS_FILE_PATH = Path(f"{BINARY_PATH}/settings.yaml")
+GADGET_PATH = Path(f"{APPLICATION_PATH}/gadgets")
+TEMPLATES_PATH = Path(f"{APPLICATION_PATH}/templates")
+
+if __name__ == "__main__":
+    subprocess.run("go build -o ./dist/gogo ./cmd/...", shell=True)
+
+    if not os.path.isdir(APPLICATION_PATH):
+        os.mkdir(APPLICATION_PATH)
+        os.mkdir(BINARY_PATH)
+        os.mkdir(GADGET_PATH)
+        os.mkdir(TEMPLATES_PATH)
+
+    shutil.move(DIST_PATH, BINARY_PATH)
+
+    with open(SETTINGS_FILE_PATH, "a") as f:
+        f.write(f'gadget-path: "{APPLICATION_PATH}/gadgets"\n')
+        f.write(f'template-path: "{APPLICATION_PATH}/templates"')
