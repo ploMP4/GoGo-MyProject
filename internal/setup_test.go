b8f5f204c019f3fb802021741c140f3417dfe91d
ploMP4 <kostasarto2@gmail.com>
2023-09-30 16:45:55 +0300

    tests: redirect color.Output to DevNull

--- internal/setup_test.go
+++ internal/setup_test.go
@@ -2,9 +2,14 @@ package internal
 
 import (
 	"os"
+	"syscall"
 	"testing"
+
+	"github.com/fatih/color"
 )
 
 func TestMain(m *testing.M) {
+	color.Output = os.NewFile(uintptr(syscall.Stdin), os.DevNull)
+
 	os.Exit(m.Run())
 }
